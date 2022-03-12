61f0d81b95cb21ed08a4a21bdf3f37a53c360775
Ashraf <zulashrafvns@gmail.com>
2022-03-12 13:22:08 +0800

    compare

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,23 +1,27 @@
 const Register = () => {
   return (
-    <div className="h-screen px-10 flex flex-col pt-[30%]">
+    <div className="h-screen px-10 flex flex-col justify-center max-w-md m-auto lg:grid grid-cols-2  lg:px-0 lg:w-full lg:max-w-full lg:overflow-hidden">
 
         {/* images */}
+        <div className="hidden lg:flex">
+            <img src="/assets/register.jpg" alt="" className="object-cover " />
+        </div>
 
         {/* form */}
-      <section className="">
-        <h1 className="text-3xl ">Welcome Back.</h1>
+      <section className="lg:p-[20%]">
+        <h1 className="text-3xl font-semibold">Welcome Back.</h1>
         <p className="text-gray-400">let's continue with us</p>
 
         <form action="" className="flex flex-col mt-10 ">
-          <input type="text" className="bg-blue-50 px-3 py-3 rounded-lg outline-none" />
+          <input type="text" className="bg-blue-50 px-3 py-3 rounded-lg outline-none" placeholder="email"/>
           <input
             type="password"
             className="bg-blue-50 px-3 py-3 rounded-lg mt-5 outline-none"
+            placeholder="password"
           />
 
           <section className="flex justify-center items-center text-sm mt-10 text-gray-400">
-            <input type="checkbox" name="agree" className="mr-3 "/>
+            <input type="checkbox" name="agree" className="mr-3"/>
             <p>
               I accept <a href="http://google.com">Terms & Condition</a>.
             </p>
@@ -27,9 +31,7 @@ const Register = () => {
             Register
           </button>
         </form>
-
       </section>
-
     </div>
   );
 };
