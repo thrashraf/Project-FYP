4e826c80c6dfa9a81ba6e151e9b03f32a73729cd
Ashraf <zulashrafvns@gmail.com>
2022-03-12 21:43:20 +0800

    fix merge

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,56 +1,31 @@
-import React from "react";
+import React from 'react'
 
-type Props = {};
+type Props = {}
 
 export const Login = (props: Props) => {
   return (
-    <div className=" h-screen px-10 flex flex-col pt-[30%]">
-      <div className="">
-        <h1 className="text-3xl">Welcome</h1>
-        <p className=" text-gray-400">Let's Login With Us</p>
+    <div className=' h-screen px-10 flex flex-col pt-[30%]'>
 
-<<<<<<< HEAD
-        <form action="" className=" flex flex-col mt-10">
-          <input
-            type="text"
-            className=" bg-blue-50 px-3 py-3 rounded-lg outline-none"
-          />
-          <input
-            type="password"
-            className=" bg-blue-50 px-3 py-3 rounded-lg mt-5 outline-none"
-          />
-          <a href="google.com" className=" text-center mt-5 text-blue-500">
-            Forgot Password?
-          </a>
-
-          <button className="mt-10 bg-blue-500 text-white px-3 py-3 rounded-lg">
-=======
 
       <div className=''>
         <h1 className=' text-3xl'>Welcome</h1>
         <p className=' text-gray-400'>Let's Login With Us</p>
 
         <form action='' className=' flex flex-col mt-10'>
-          <input type='text' className=' bg-blue-50 px-3 py-3 rounded-lg outline-none' />
+          <input type='text' className=' bg-blue-50 px-3 py-3 rounded-lg outline-none'/>
           <input
-            type='password'
-            className=' bg-blue-50 px-3 py-3 rounded-lg mt-5 outline-none' />
-          <a href='' className=' text-center mt-5 text-blue-500'>Forgot Password?</a>
-
+          type='password'
+          className=' bg-blue-50 px-3 py-3 rounded-lg mt-5 outline-none'/>
+            <a href='' className=' text-center mt-5 text-blue-500'>Forgot Password?</a>
+          
           <button className='mt-10 bg-blue-500 text-white px-3 py-3 rounded-lg'>
->>>>>>> 4aa46dc7c5638ae795be996b7e81db7d2d78912d
             Login
-          </button>
-        </form>
+            </button>
+            </form>
+
+
       </div>
-<<<<<<< HEAD
-    </div>
-  );
-};
-=======
 
-    </div>
+      </div>
 
-  )
-}
->>>>>>> 4aa46dc7c5638ae795be996b7e81db7d2d78912d
+  )}
