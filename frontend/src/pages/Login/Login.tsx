f6c725d10fda03e633a6c2b5c45ee1777bb72382
Isokayy <fareezdaniel34@gmail.com>
2022-03-17 23:02:36 +0800

    fix login page

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,25 +1,24 @@
-import React from "react";
-
-type Props = {};
-
-export const Login = (props: Props) => {
+import { Image } from "./Image"
+export const Login = () => {
   return (
-    <div className=" h-screen px-10 flex flex-col pt-[30%]">
-      <div className="">
+    <div className=" h-screen px-10 flex flex-col justify-center max-w-md m-auto lg:grid grid-cols-2  lg:px-0 lg:w-full lg:max-w-[1600px] lg:overflow-hidden ">
+      <div className=" lg:p-[20%]">
         <h1 className=" text-3xl">Welcome</h1>
         <p className=" text-gray-400">Let's Login With Us</p>
 
         <form action="" className=" flex flex-col mt-10">
           <input
             type="text"
-            className=" bg-blue-50 px-3 py-3 rounded-lg outline-none"
+            placeholder="Email"
+            className=" bg-blue-100 px-3 py-3 rounded-lg outline-none"
           />
           <input
             type="password"
-            className=" bg-blue-50 px-3 py-3 rounded-lg mt-5 outline-none"
+            placeholder="Password"
+            className=" bg-blue-100 px-3 py-3 rounded-lg mt-5 outline-none"
           />
           <a
-            href="http://google.com"
+            href=""
             className=" text-center mt-5 text-blue-500"
           >
             Forgot Password?
@@ -30,6 +29,7 @@ export const Login = (props: Props) => {
           </button>
         </form>
       </div>
+      <Image/>
     </div>
   );
 };
