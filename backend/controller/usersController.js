073db3b2c879eaa5e81119b2a5baff66994c346f
Isokayy <fareezdaniel34@gmail.com>
2022-03-18 21:54:55 +0800

    add login function

--- backend/controller/usersController.js
+++ backend/controller/usersController.js
@@ -1,37 +1,47 @@
 import user from "../model/users.js";
 import crypto from "crypto";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 import bcrypt from "bcryptjs";
+dotenv.config()
+
 
 export const registerUser = async (req, res) => {
   try {
+    const { firstName, lastName, email, password } = req.body;
 
-    const {
-      firstName,
-      lastName,
-      email,
-      password
-    } = req.body;
-
+    //to generate unique id
     const id = crypto.randomBytes(16).toString("hex");
 
-    const [checkExistingEmail] = await user.checkEmail(email);
+    await user.register(id, firstName, email, password);
 
-    if (checkExistingEmail.length > 0) {
-      //console.log('email already exist');
+    res.status(200).json({ message: 'successful create!' })
+
+  } catch (error) { }
+
+};
+
+
+
+export const loginuser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const [checkExistingEmail] = await user.checkEmail(email);
+    
+
+    if(!checkExistingEmail){
       return res.status(400).json({
-        message: "email already exist"
-      });
+        message:"Incorrect Email or Password"
+      })
     }
 
-    const hashPassword = bcrypt.hashSync(password);
-
-    await user.register(id, firstName, email, hashPassword);
-
-    res.status(200).json({
-      message: "successful create!",
-    });
-
+    //User info
+    const userInfo = checkExistingEmail[0]
+    const isvalid = bcrypt.compareSync(password, userInfo.password)
+    
+    const token = jwt.sign({id: userInfo.id}, process.env.TOKEN_SECRET)
+    
   } catch (error) {
-    console.log(error);
+
   }
-};
+}
