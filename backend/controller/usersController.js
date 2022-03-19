42bfc78b80947e326975b51872c23a13ff9d0978
Isokayy <fareezdaniel34@gmail.com>
2022-03-19 23:33:42 +0800

    add token

--- backend/controller/usersController.js
+++ backend/controller/usersController.js
@@ -1,10 +1,9 @@
 import user from "../model/users.js";
 import crypto from "crypto";
-import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
-dotenv.config()
-
+dotenv.config();
 
 export const registerUser = async (req, res) => {
   try {
@@ -15,33 +14,48 @@ export const registerUser = async (req, res) => {
 
     await user.register(id, firstName, email, password);
 
-    res.status(200).json({ message: 'successful create!' })
-
-  } catch (error) { }
-
+    res.status(200).json({ message: "successful create!" });
+  } catch (error) {}
 };
-
-
 
 export const loginuser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const [checkExistingEmail] = await user.checkEmail(email);
-    
 
-    if(!checkExistingEmail){
+    if (!checkExistingEmail) {
       return res.status(400).json({
-        message:"Incorrect Email or Password"
-      })
+        message: "Incorrect Email or Password",
+      });
     }
 
     //User info
-    const userInfo = checkExistingEmail[0]
-    const isvalid = bcrypt.compareSync(password, userInfo.password)
-    
-    const token = jwt.sign({id: userInfo.id}, process.env.TOKEN_SECRET)
-    
-  } catch (error) {
+    const userInfo = checkExistingEmail[0];
+    const isValid = bcrypt.compareSync(password, userInfo.password);
 
+    // if(!isValid){
+    //   return res.status(400).json({
+    //     message: "Incorrect Email or Password"
+    //   })
+    // }
+
+    const accessToken = jwt.sign(
+      { id: userInfo.id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "20s",
+      }
+    );
+    console.log(accessToken);
+    const refreshToken = jwt.sign(
+      { id: userInfo.id },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.status(200).json({ accessToken });
+  } catch (error) {
+    res.status(404).json({ msg: "Email Not Found" });
   }
-}
+};
