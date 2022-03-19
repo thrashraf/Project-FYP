17e62761b8904aba55adfdd181995b0c65e4911d
Ashraf <zulashrafvns@gmail.com>
2022-03-19 13:43:14 +0800

    fix conflict

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,26 +1,13 @@
 import { Image } from "./Image"
 import { Form } from "./Form";
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 3f3ebadd462a45e2aafbcd1588606f1ecc6f0c69
-=======
-
->>>>>>> 07bc15ee4820c63a48709c49cad8b628dc750605
 export const Login = () => {
   return (
     <div className=" h-screen px-10 flex flex-col justify-center max-w-md m-auto lg:grid grid-cols-2  lg:px-0 lg:w-full lg:max-w-[1600px] lg:overflow-hidden ">
       <div className=" lg:p-[20%]">
         <h1 className=" text-3xl">Welcome</h1>
         <p className=" text-gray-400">Let's Login With Us</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 3f3ebadd462a45e2aafbcd1588606f1ecc6f0c69
-=======
->>>>>>> 07bc15ee4820c63a48709c49cad8b628dc750605
         <Form />
       </div>
       <Image/>
