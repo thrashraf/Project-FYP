7bf2b2addded561d7e239668f2d9259725c4167b
Isokayy <fareezdaniel34@gmail.com>
2022-03-30 21:00:14 +0800

    update body

--- frontend/src/pages/Home/Body.tsx
+++ frontend/src/pages/Home/Body.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 export const Body = () => {
     return(
-        <div className='max-w-[1240px] mx-auto '>
-           <div className=' text-center'>
-            <h1 className=' text-[16px] underline font-bold border-separate  font-serif'>System Activity Management System</h1>
+        <div >
+            
         </div>
-        </div>
+        
     )
 }
