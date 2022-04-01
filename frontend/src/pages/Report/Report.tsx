4a5b9c8315162201829a9d69f0989069c36b1040
Ashraf <zulashrafvns@gmail.com>
2022-04-02 03:12:21 +0800

    add upload images functionality

--- frontend/src/pages/Report/Report.tsx
+++ frontend/src/pages/Report/Report.tsx
@@ -1,12 +1,11 @@
 import React, { useRef, useState } from "react";
 import { Input } from "../../components/Input";
 import { Template } from "./Template";
-import { Image } from '../../icons/Image';
+import { Image } from "../../icons/Image";
+import axios from "axios";
 
-type Props = {};
 
-const Report = (props: Props) => {
-  
+const Report = () => {
   const [title, setTitle] = useState<string>("");
   const [date, setDate] = useState<string>("");
   const [content, setContent] = useState<string>("");
@@ -15,30 +14,38 @@ const Report = (props: Props) => {
   const [venue, setVenue] = useState<string>("");
 
   const [isPhoto, setIsPhoto] = useState<boolean>(false);
-  const [photo, setPhoto] = useState<string []>([]);
+  const [photo, setPhoto] = useState<any []>([]);
 
   const uploadRef = useRef<HTMLInputElement>(null);
 
   const contentHandler = (e: any) => {
-    if (e.key === 'Enter') {
-      setContent(`${e.target.value}\n`)
+    if (e.key === "Enter") {
+      setContent(`${e.target.value}\n`);
     }
-    setContent(e.target.value)
-  }
+    setContent(e.target.value);
+  };
 
   const uploadFile = () => {
     if (uploadRef.current !== null) {
-      uploadRef.current.click()
-    } 
-  }
+      uploadRef.current.click();
+    }
+  };
 
   const fileSelectorHandler = (e: any) => {
-    console.log(e.target.files);
-    setPhoto(photo => [...photo, e.target.files])
-    console.log(photo);
-  }
+    const tempArr: any = [];
 
-  
+    console.log(e.target.files);
+    [...e.target.files].map((file) => {
+      return tempArr.push({
+        data: file,
+        url: URL.createObjectURL(file),
+      });
+    });
+
+    setPhoto(tempArr);
+    setIsPhoto(true)
+  };
+
 
   return (
     <div className="lg:grid grid-cols-2  ">
@@ -111,62 +118,98 @@ const Report = (props: Props) => {
             />
           </section>
 
-          <section className="my-5 flex items-center text-blue-400 cursor-pointer 
-          " onClick={uploadFile}>  
-              <div className="px-3 rounded-md bg-blue-50">
-                <Image/>
-              </div>
-              <p className="ml-5 font-medium">Upload File</p>
-              <input type="file" accept="image/*" multiple={true} className="hidden" ref={uploadRef} onChange={(e) => fileSelectorHandler(e)}/>
+          <section
+            className="my-5 flex items-center text-blue-400 cursor-pointer 
+          "
+            onClick={uploadFile}
+          >
+            <div className="px-3 rounded-md bg-blue-50">
+              <Image />
+            </div>
+            <p className="ml-5 font-medium">Upload File</p>
+            <input
+              type="file"
+              accept="image/*"
+              multiple={true}
+              name="upload"
+              className="hidden"
+              ref={uploadRef}
+              onChange={(e) => fileSelectorHandler(e)}
+            />
           </section>
-
-
         </div>
       </section>
 
       <section className="hidden lg:flex flex-col bg-gray-500 pb-10">
-        
-        <Template title={title} content={content} name={programName} organizer={organizer} date={date} venue={venue} />
-
+        <Template
+          title={title}
+          content={content}
+          name={programName}
+          organizer={organizer}
+          date={date}
+          venue={venue}
+        />
         {content.length > 260 ? (
-              <div className="my-2.5 w-[500px] h-[600px] font-serif m-auto bg-white rounded-sm p-10 flex flex-col text-[10px] relative">
-              {content.slice(260,content.length).split("\n").map((text, index) => {
-                  console.log(text.length)
-                  return (
-                    <p key={index}  className=" indent-8 mt-5 text-justify">{text}</p>
-                    )
-                  })}
+          <div className="my-2.5 w-[500px] h-[600px] font-serif m-auto bg-white rounded-sm p-10 flex flex-col text-[10px] relative">
+            {content
+              .slice(260, content.length)
+              .split("\n")
+              .map((text, index) => {
+                console.log(text.length);
+                return (
+                  <p key={index} className=" indent-8 mt-5 text-justify">
+                    {text}
+                  </p>
+                );
+              })}
 
-              <section className={`mt-10 ${content.length > 260 ? null : 'hidden'} absolute bottom-10`} >
-                <p>Disediakan oleh: </p>
-                <div className=" border-b-2 border-dotted border-black w-[80px] mt-2 h-[30px]">
-                    <img src="/assets/signature.png" alt="signature" className="object-cover h-[30px] mx-auto" />
-                </div>
-                <section className="text-[8px]">
-                    <p>(MUHAMMAD ZULASRAF BIN ZULKIFLI)</p>
-                    <p>(PENGARAH)</p> 
-                </section>
+            <section
+              className={`mt-10 ${
+                content.length > 260 ? null : "hidden"
+              } absolute bottom-10`}
+            >
+              <p>Disediakan oleh: </p>
+              <div className=" border-b-2 border-dotted border-black w-[80px] mt-2 h-[30px]">
+                <img
+                  src="/assets/signature.png"
+                  alt="signature"
+                  className="object-cover h-[30px] mx-auto"
+                />
+              </div>
+              <section className="text-[8px]">
+                <p>(MUHAMMAD ZULASRAF BIN ZULKIFLI)</p>
+                <p>(PENGARAH)</p>
+              </section>
             </section>
-            </div>
+          </div>
         ) : null}
-
-        <div className={`my-2.5 w-[500px] h-[600px] font-serif m-auto bg-white rounded-sm p-10 flex flex-col text-[10px] relative ${!isPhoto && 'hidden'}`}>
-              
+        <div
+          className={`my-2.5 w-[500px] h-[600px] font-serif m-auto bg-white rounded-sm p-10 flex flex-col text-[10px] relative ${
+            !isPhoto && "hidden"
+          }`}
+        >
+          <h1 className="font-bold">GAMBAR GAMBAR SEPANJANG AKTIVITI</h1>
+          <section className="grid grid-cols-3 gap-5 mt-10">
+            {photo?.map((img, index) => {
+              return(
+                <img src={`${img.url}`} alt="pho" key={index} className="w-[100px] h-[100px] object-cover m-auto"/>
+              )
+            })}
+          </section>
         </div>
-        
-      )
-        
+        )
         <section className="mt-10 flex justify-end mr-10">
-          <button className="mt-10 bg-green-500 text-white px-3 py-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-green-400 cursor-pointer w-[100px] mr-5">Submit</button>
+          <button className="mt-10 bg-green-500 text-white px-3 py-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-green-400 cursor-pointer w-[100px] mr-5">
+            Submit
+          </button>
 
-          <button className="mt-10 bg-blue-500 text-white px-3 py-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-400 cursor-pointer w-[100px] ">Download</button>
+          <button className="mt-10 bg-blue-500 text-white px-3 py-2 rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-400 cursor-pointer w-[100px] ">
+            Download
+          </button>
         </section>
-
       </section>
     </div>
   );
 };
-
-
 
 export default Report;
